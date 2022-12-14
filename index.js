84376b1e8becfeface317d78b5e237b77f77f6ba
Abdelrahman Zaki <61281253+AbdelrahmanZakiCs@users.noreply.github.com>
2022-12-14 15:28:57 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $(".slide").slideDown(2000);
+    });
+});
+
+
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $(".Conference").slideDown(3000);
+    });
+});
+
+
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $(".board").slideDown(5000);
+    });
+});
+
